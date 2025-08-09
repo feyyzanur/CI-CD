8bed3dbef247f81b5029ad3ae980611e82186fdf
feyyzanur <112513981+feyyzanur@users.noreply.github.com>
2025-08-10 01:12:45 +0300

    dosyalar güncellendi

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
+  e2e: { supportFile: "cypress/support/e2e.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
